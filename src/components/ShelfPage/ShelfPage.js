b5e9ca0bcb7733d4da8880114a813d1c21eb8f09
Burner <10004812+BurnerWah@users.noreply.github.com>
2023-12-14 13:20:42 -0600

    Added delete button to shelf page

--- src/components/ShelfPage/ShelfPage.js
+++ src/components/ShelfPage/ShelfPage.js
@@ -6,10 +6,15 @@ function ShelfPage() {
   const dispatch = useDispatch();
 
   const shelf = useSelector((store) => store.shelf);
+  const user = useSelector((store) => store.user);
 
   useEffect(() => {
     dispatch({ type: 'FETCH_SHELF' });
   }, []);
+
+  const deleteShelf = (id) => {
+    dispatch({ type: 'DELETE_SHELF', payload: id });
+  };
 
   return (
     <div className="container">
@@ -21,6 +26,7 @@ function ShelfPage() {
           <tr>
             <th>Description</th>
             <th>Image</th>
+            <th>Remove</th>
           </tr>
         </thead>
         <tbody>
@@ -29,6 +35,14 @@ function ShelfPage() {
               <td>{item.description}</td>
               <td>
                 <img src={item.image_url} />
+              </td>
+              <td>
+                {
+                  /* Only show the delete button if the user ID is the same as whoever created this */
+                  item.user_id === user.id && (
+                    <button onClick={() => deleteShelf(item.id)}>Delete</button>
+                  )
+                }
               </td>
             </tr>
           ))}
