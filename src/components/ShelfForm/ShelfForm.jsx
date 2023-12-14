4bbc23e41de24009980e2ed056bb17713a4255dd
Burner <10004812+BurnerWah@users.noreply.github.com>
2023-12-14 11:20:46 -0600

    Added code to submit stuff to the server

--- src/components/ShelfForm/ShelfForm.jsx
+++ src/components/ShelfForm/ShelfForm.jsx
@@ -1,13 +1,34 @@
 import React, { useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 
 export default function ShelfForm() {
+  const dispatch = useDispatch();
+
+  const user = useSelector((store) => store.user);
+
   const [description, setDescription] = useState('');
   const [image_url, setImage_url] = useState('');
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    dispatch({
+      type: 'SUBMIT_SHELF',
+      payload: {
+        description: description,
+        image_url: image_url,
+        user_id: user.id,
+      },
+    });
+
+    setDescription('');
+    setImage_url('');
+  };
 
   return (
     <div>
       <h2>Shelf Form</h2>
-      <form>
+      <form onSubmit={handleSubmit}>
         <input
           type="text"
           placeholder="description"
