916fb557ef3db36f6ec807650d418b68fd99c6ee
Burner <10004812+BurnerWah@users.noreply.github.com>
2023-12-14 10:33:14 -0600

    Made shelf saga set the shelf data

--- src/redux/sagas/shelf.saga.js
+++ src/redux/sagas/shelf.saga.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-import { takeLatest } from 'redux-saga/effects';
+import { put, takeLatest } from 'redux-saga/effects';
 
 function* fetchShelf() {
   try {
@@ -9,6 +9,8 @@ function* fetchShelf() {
     };
 
     const response = yield axios.get('/api/shelf', config);
+
+    yield put({ type: 'SET_SHELF', payload: response.data });
   } catch (error) {
     console.log('Shelf get request failed', error);
   }
