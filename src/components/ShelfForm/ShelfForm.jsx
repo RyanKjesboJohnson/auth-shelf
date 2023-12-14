c5ad629c95be478f9553846b740a1d44d5495182
Burner <10004812+BurnerWah@users.noreply.github.com>
2023-12-14 11:09:23 -0600

    Added some input fields to the form

--- src/components/ShelfForm/ShelfForm.jsx
+++ src/components/ShelfForm/ShelfForm.jsx
@@ -1,9 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 export default function ShelfForm() {
+  const [description, setDescription] = useState('');
+  const [image_url, setImage_url] = useState('');
+
   return (
     <div>
       <h2>Shelf Form</h2>
+      <form>
+        <input
+          type="text"
+          placeholder="description"
+          value={description}
+          onChange={(event) => setDescription(event.target.value)}
+        />
+        <input
+          type="text"
+          placeholder="image_url"
+          value={image_url}
+          onChange={(event) => setImage_url(event.target.value)}
+        />
+        <button type="submit">Submit</button>
+      </form>
     </div>
   );
 }
